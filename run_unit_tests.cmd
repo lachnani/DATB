0a28093b65b1a6e8e4d4cdf48bd3e57415009e40
Hakim Lachnani <43235050+lachnani@users.noreply.github.com>
2026-01-25 19:43:49 -0500

    Generalizing unit tests command

--- run_unit_tests.cmd
+++ run_unit_tests.cmd
@@ -1,7 +1,7 @@
-python -m unittest discover C:\Sandbox\srm\src\datb\dynamics
+python -m unittest discover dynamics
 
-python -m unittest discover C:\Sandbox\srm\src\datb\guidance
+python -m unittest discover guidance
 
-python -m unittest discover C:\Sandbox\srm\src\datb\planning
+python -m unittest discover planning
 
 pause
